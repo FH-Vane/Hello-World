7d9e56c29c13cf71f5bf05f51dee5eabd8756c30
FH-Vane <69616379+FH-Vane@users.noreply.github.com>
2020-08-12 21:56:04 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,10 +10,10 @@ public class Main
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("My name is Fengyi Huang, although I am fine with nicknames, and I am a junior at MHS");
-		System.out.println("My hobbies are playing video games, hiking, and having fun with friends");
-		System.out.println("My favorite video games are Starcraft, League of Legends, and Minecraft");
-		System.out.println("I am considering majoring in mathematics, computer science, or business");
-		System.out.println("My favorite subjects in school are mathematics and science(computer science is also a favorite of mine)");
+		System.out.println("My name is Fengyi Huang, although I am fine with nicknames, and I am a junior at MHS.");
+		System.out.println("My hobbies are playing video games, hiking, and having fun with friends.");
+		System.out.println("My favorite video games are Starcraft, League of Legends, and Minecraft.");
+		System.out.println("I am considering majoring in mathematics, computer science, or business.");
+		System.out.println("My favorite subjects in school are mathematics and science(computer science is also a favorite of mine).");
 	}
 }
