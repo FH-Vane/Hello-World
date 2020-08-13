8049e83c2df415a80fed688d3524563aca145dea
FH-Vane <69616379+FH-Vane@users.noreply.github.com>
2020-08-12 21:52:57 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Fengyi Huang, although I am fine with nicknames, and I am a junior at MHS");
+		System.out.println("My hobbies are playing video games, hiking, and having fun with friends");
+		System.out.println("My favorite video games are Starcraft, League of Legends, and Minecraft");
+		System.out.println("I am considering majoring in mathematics, computer science, or business");
+		System.out.println("My favorite subjects in school are mathematics and science(computer science is also a favorite of mine)");
+	}
+}
